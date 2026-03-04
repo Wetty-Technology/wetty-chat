c35bf5a71c17b4db74df31701f91cf1cb14473e1
Codetector <codetector@codetector.org>
2026-03-03 16:55:25 -0800

    feat/frontend: Implement PWA

--- wetty-chat-mobile/vite.config.ts
+++ wetty-chat-mobile/vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { lingui } from '@lingui/vite-plugin';
+import { VitePWA } from 'vite-plugin-pwa';
 import dotenv from 'dotenv';
 import { defineConfig } from 'vite';
 
@@ -20,6 +21,34 @@ export default defineConfig({
       },
     }),
     lingui(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Wetty Chat',
+        short_name: 'W Chat',
+        description: 'Wetty Chat',
+        start_url: 'https://wchat.i386.mov',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'appicon/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'appicon/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        navigateFallbackDenylist: [/^\/_api/],
+      }
+    })
   ],
   resolve: {
     alias: {
