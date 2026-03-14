13db0ed15c92e3cda5b613a803ee9371149898e4
Codetector <codetector@codetector.org>
2026-03-13 23:37:19 -0700

    frontend/bugfix: i18n in html tag

--- wetty-chat-mobile/src/i18n.ts
+++ wetty-chat-mobile/src/i18n.ts
@@ -5,6 +5,7 @@ export async function dynamicActivate(locale: string) {
   const { messages } = await import(`../locales/${locale}/messages.po`);
   i18n.load(locale, messages);
   i18n.activate(locale);
+  document.documentElement.lang = locale;
 }
 
 export async function activateDetectedLocale() {
