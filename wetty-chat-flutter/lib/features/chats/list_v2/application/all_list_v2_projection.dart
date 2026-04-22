70d2166401505b5b813662be41d03c449855efb0
jue ma <ykhinama@gmail.com>
2026-04-22 12:20:41 -0700

    refactor: optimize list merging by replacing sort with a linear merge algorithm

--- wetty-chat-flutter/lib/features/chats/list_v2/application/all_list_v2_projection.dart
+++ wetty-chat-flutter/lib/features/chats/list_v2/application/all_list_v2_projection.dart
@@ -12,16 +12,37 @@ final allListV2ItemsProvider = Provider<List<AllListV2Item>>((ref) {
     threadListV2StoreProvider.select((state) => state.threads),
   );
 
-  final items = <AllListV2Item>[
-    for (final group in groups) AllGroupListV2Item(group),
-    for (final thread in threads) AllThreadListV2Item(thread),
-  ];
+  final items = <AllListV2Item>[];
+  var groupIndex = 0;
+  var threadIndex = 0;
 
-  items.sort((a, b) {
-    final aTime = a.activityAt ?? DateTime.fromMillisecondsSinceEpoch(0);
-    final bTime = b.activityAt ?? DateTime.fromMillisecondsSinceEpoch(0);
-    return bTime.compareTo(aTime);
-  });
+  while (groupIndex < groups.length && threadIndex < threads.length) {
+    final groupItem = AllGroupListV2Item(groups[groupIndex]);
+    final threadItem = AllThreadListV2Item(threads[threadIndex]);
+    final groupTime =
+        groupItem.activityAt ?? DateTime.fromMillisecondsSinceEpoch(0);
+    final threadTime =
+        threadItem.activityAt ?? DateTime.fromMillisecondsSinceEpoch(0);
+
+    if (!groupTime.isBefore(threadTime)) {
+      items.add(groupItem);
+      groupIndex += 1;
+      continue;
+    }
+
+    items.add(threadItem);
+    threadIndex += 1;
+  }
+
+  while (groupIndex < groups.length) {
+    items.add(AllGroupListV2Item(groups[groupIndex]));
+    groupIndex += 1;
+  }
+
+  while (threadIndex < threads.length) {
+    items.add(AllThreadListV2Item(threads[threadIndex]));
+    threadIndex += 1;
+  }
 
   return List<AllListV2Item>.unmodifiable(items);
 });
