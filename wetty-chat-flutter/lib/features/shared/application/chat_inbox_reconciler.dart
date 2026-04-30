6af0c760b5df408921e898a490ef2629e83f1ae4
jue ma <ykhinama@gmail.com>
2026-04-30 13:33:12 -0700

    flutter/feat: Improve chat inbox reconciliation by refreshing archived threads and adjusting refresh limits

--- wetty-chat-flutter/lib/features/shared/application/chat_inbox_reconciler.dart
+++ wetty-chat-flutter/lib/features/shared/application/chat_inbox_reconciler.dart
@@ -2,8 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:chahua/core/notifications/unread_badge_provider.dart';
 import 'package:chahua/core/session/dev_session_store.dart';
+import 'package:chahua/features/chat_list/application/thread_list_v2_store.dart';
 import 'package:chahua/features/chat_list/application/group_list_v2_view_model.dart';
 import 'package:chahua/features/chat_list/application/thread_list_v2_view_model.dart';
+import 'package:chahua/features/chat_list/data/thread_list_v2_repository.dart';
 
 class ChatInboxReconciler {
   ChatInboxReconciler(this._ref);
@@ -54,11 +56,24 @@ class ChatInboxReconciler {
     final current = _ref.read(activeThreadListV2ViewModelProvider).value;
     if (current == null) {
       await _ref.read(activeThreadListV2ViewModelProvider.future);
+    } else {
+      await _ref
+          .read(activeThreadListV2ViewModelProvider.notifier)
+          .refreshThreads();
+    }
+
+    final archived = _ref.read(threadListV2StoreProvider).archived;
+    if (!archived.isLoaded) {
       return;
     }
+
     await _ref
-        .read(activeThreadListV2ViewModelProvider.notifier)
-        .refreshThreads();
+        .read(threadListV2RepositoryProvider)
+        .loadArchivedThreads(limit: _refreshLimit(archived));
+  }
+
+  int _refreshLimit(ThreadListV2ListState listState) {
+    return listState.threads.isEmpty ? 20 : listState.threads.length;
   }
 }
 
